0434f5f3c1413b8afb28c53c718a8e327f8e577f
Nikul-OSI <nchaudhary@opensourceintegrators.com>
2024-07-03 15:31:33 +0530

    [FIX] Fixed code

--- account_avatax_exemption/models/res_country_state.py
+++ account_avatax_exemption/models/res_country_state.py
@@ -5,3 +5,14 @@ class CountryState(models.Model):
     _inherit = "res.country.state"
 
     rule_ids = fields.One2many("exemption.code.rule", "state_id", "Avatax Rules")
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    use_commercial_entity = fields.Boolean(compute="_compute_use_commercial_entity")
+
+    def _compute_use_commercial_entity(self):
+        avalara_salestax = self.env["avalara.salestax"].sudo().search([], limit=1)
+        for partner in self:
+            partner.use_commercial_entity = avalara_salestax.use_commercial_entity
