fa74f53bb9aa685f389dcfb2adfbd9d13bb86ba0
cbeddies <cbeddies@opensourceintegrators.com>
2025-05-02 17:43:13 -0400

    [UPD] performance change, move first call of new code to an else statement to prevent duplicate runs

--- account_avatax_oca/models/account_move.py
+++ account_avatax_oca/models/account_move.py
@@ -241,19 +241,6 @@ class AccountMove(models.Model):
             return tax_result
         # Odoo incorrectly calculates taxes returned from Avatax due to rounding errors
         # below code forces tax to equal what came over from Avatax
-        for line in self.line_ids:
-            if line.tax_line_id:  # Is a tax line
-                line_ids = self.line_ids.filtered(
-                    lambda x: line.tax_line_id in x.tax_ids and x.avatax_amt_line
-                )
-                balance = sum(line_id.amount_currency for line_id in line_ids)
-                sign = 1 if balance >= 0 else -1
-                total_tax_amount = sum(line_id.avatax_amt_line for line_id in line_ids)
-                if line_ids:
-                    vals = {
-                        "balance": sign * total_tax_amount,
-                    }
-                    line.update(vals)
         if self.state == "draft":
             Tax = self.env["account.tax"]
             tax_result_lines = {int(x["lineNumber"]): x for x in tax_result["lines"]}
@@ -324,6 +311,20 @@ class AccountMove(models.Model):
                 self.invoice_line_ids.write({"tax_ids": [(6, 0, [])]})
                 for line in self.invoice_line_ids:
                     line.write({"tax_ids": taxes_data[line.id].ids})
+        else:
+            for line in self.line_ids:
+                if line.tax_line_id:  # Is a tax line
+                    line_ids = self.line_ids.filtered(
+                        lambda x: line.tax_line_id in x.tax_ids and x.avatax_amt_line
+                    )
+                    balance = sum(line_id.amount_currency for line_id in line_ids)
+                    sign = 1 if balance >= 0 else -1
+                    total_tax_amount = sum(line_id.avatax_amt_line for line_id in line_ids)
+                    if line_ids:
+                        vals = {
+                            "balance": sign * total_tax_amount,
+                        }
+                        line.update(vals)
         return tax_result
 
     # Same as v13
