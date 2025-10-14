bb46ec137eed22ae34657748d3d407ed4dc5d21a
dbrogan-osi <dbrogan@opensourceintegrators.com>
2025-10-14 18:45:51 +0000

    Fix for ticket 67354: Submodule update
    Automated changes for ticket resolution

--- account_fiscal_position_type/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import res_partner
-from . import account_chart_template
-from . import account_fiscal_position
-from . import account_fiscal_position_template
-from . import account_invoice
--- account_fiscal_position_usage_group/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import account_fiscal_position
-from . import account_fiscal_position_template
-from . import account_invoice
-from . import res_partner
